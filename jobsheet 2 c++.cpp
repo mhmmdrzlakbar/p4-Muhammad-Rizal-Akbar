ed8359d2de753a583a7259ed828e97947eb00186
Muhammad Rizal Akbar <64632266+mhmmdrzlakbar@users.noreply.github.com>
2020-05-17 21:22:37 +0700

    Create jobsheet 2 c++.cpp
    Mengisi dan Menampilkan Elemen Aray(Dinamis)

---
+++ jobsheet 2 c++.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+main(){
+
+  int nim[8];
+  cout<<"Masukkan nilai 1 : "; 
+  cin >>nim[0];
+  cout<<"Masukkan nilai 2 : "; 
+  cin >>nim[1];
+  cout<<"Masukkan nilai 3 : "; 
+  cin >>nim[2];
+  cout<<"Masukkan nilai 4 : "; 
+  cin >>nim[3];
+  cout<<"Masukkan nilai 5 : "; 
+  cin >>nim[4];
+  cout<<"Masukkan nilai 6 : "; 
+  cin >>nim[5];
+  cout<<"Masukkan nilai 7 : "; 
+  cin >>nim[6];
+  cout<<"Masukkan nilai 8 : ";
+  cin >>nim[7];
+  cout<<endl;
+
+  cout<<"Nilai ke-1 = "<<nim[0]<<endl;
+  cout<<"Nilai ke-2 = "<<nim[1]<<endl;
+  cout<<"Nilai ke-3 = "<<nim[2]<<endl;
+  cout<<"Nilai ke-4 = "<<nim[3]<<endl;
+  cout<<"Nilai ke-5 = "<<nim[4]<<endl;
+  cout<<"Nilai ke-6 = "<<nim[5]<<endl;
+  cout<<"Nilai ke-7 = "<<nim[6]<<endl;
+  cout<<"Nilai ke-8 = "<<nim[7]<<endl;
+  system("PAUSE");
+
+}
