78223be7703f86661c79e14fd4fbd204be06a77a
Muhammad Rizal Akbar <64632266+mhmmdrzlakbar@users.noreply.github.com>
2020-05-17 21:22:09 +0700

    Create jobsheet 1 c++.cpp
    Menampilkan array satu dimensi

---
+++ jobsheet 1 c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main(){
+  int nim[5]= {1,0,17,0,68};
+  char huruf[2]={'F','B'};
+
+  cout<<"Nama : Muhammad Rizal Akbar \n";
+  cout<<"NIM : ";
+  cout<<("\ndata ke-0= ",huruf[0]);
+  cout<<("\ndata ke-1= ",nim[0]);
+  cout<<("\ndata ke-1= ",huruf[1]);
+  cout<<("\ndata ke-2= ",nim[1]);
+  cout<<("\ndata ke-3= ",nim[2]);
+  cout<<("\ndata ke-4= ",nim[3]);
+  cout<<("\ndata ke-5= ",nim[4]);
+  cout<<"\nKelompok : 3";
+}
