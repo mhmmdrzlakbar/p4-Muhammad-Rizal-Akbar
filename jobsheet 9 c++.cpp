3fcf39abbb5cb0ec9995840028f8e19e341224d2
Muhammad Rizal Akbar <64632266+mhmmdrzlakbar@users.noreply.github.com>
2020-05-17 21:26:19 +0700

    Create jobsheet 9 c++.cpp
    Array sebagai Karakter

---
+++ jobsheet 9 c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int i;
+char nama[6]={'A','K','B','A','R','\0'};
+char nama2[6]="AKBAR";
+cout<<"Array per karakter =";
+for(i-0;i<=5;i++){
+cout<<nama[i];
+}
+cout<<endl;
+cout<<"Array string = "<<nama2<<endl;
+}
